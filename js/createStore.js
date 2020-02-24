44b9f6fdbc173da206db80cb44ee19c76d42566e
jessrezac <jess.rezac@gmail.com>
2020-02-24 15:12:06 -0600

    Done.

--- js/createStore.js
+++ js/createStore.js
@@ -14,14 +14,22 @@ function createStore(reducer) {
   
 }
 
+function reducer(state = { count: 0 }, action) {
+  switch (action.type) {
+    case 'INCREASE_COUNT':
+      return { count: state.count + 1 };
 
+    default:
+      return state;
+  }
+};
 
 function render() {
   let container = document.getElementById('container');
   container.textContent = store.getState().count;
 };
 
-let store = createStore()
+let store = createStore(reducer)
 store.dispatch({ type: '@@INIT' })
 let button = document.getElementById('button');
 button.addEventListener('click', function() {
